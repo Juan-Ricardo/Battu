e1d5619e65dc5639103374e0a750a07f4bfa8a01
Ricardo <ricardo.ramos@unas.edu.pe>
2018-03-23 15:09:01 -0500

    One Commit.

---
+++ app/src/main/java/com/battu/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.battu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/battu/notification/FirebaseCloudMessaging.java
@@ -0,0 +1,8 @@
+package com.battu.notification;
+
+/**
+ * Created by Alfredo Ramos on 25/11/2017.
+ */
+
+public class FirebaseCloudMessaging {
+}
